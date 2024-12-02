54ec7bb9c80a61de3d7f3be95089a692de1a4aaa
zzyzek <zzyzek@thumpernet.com>
2024-12-02 16:59:51 -0500

    z/time component in example

--- sokoita/src/example_run.sh
+++ sokoita/src/example_run.sh
@@ -8,5 +8,6 @@ node sokoita_rulegen.js \
   -t ../img/sokoita_flat_tileset.png \
   -M sokoita_tiled.json \
   -m sokoita_flat_tiled.json \
+  -z 64 \
   -s 16 \
   -P sokoita_poms_xsokoban90_1.json
