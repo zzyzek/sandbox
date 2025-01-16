7761c485c15fcd95a90730f5169b5d917476408d
zzyzek <zzyzek@thumpernet.com>
2025-01-15 23:22:55 -0500

    wip

--- gilbert-figs/js/fig.js
+++ gilbert-figs/js/fig.js
@@ -1550,20 +1550,17 @@ function _init() {
   postProcess();
 }
 
-function init() {
+function mk_checkerboard_path(opt) {
   let two = g_fig_ctx.two;
 
-  var ele = document.getElementById("fig");
-  two.appendTo(ele);
+  let sx = opt.sx;
+  let sy = opt.sy;
 
-  let sx = 50;
-  let sy = 50;
+  let nx = opt.nx;
+  let ny = opt.ny;
 
-  let nx = 4;
-  let ny = 4;
-
-  let x_len = 30;
-  let y_len = 30;
+  let x_len = opt.x_len;
+  let y_len = opt.y_len;
 
   let color_scheme = {
     "red-beige": [ [248,112,116], [248,235,187] ],
@@ -1573,33 +1570,321 @@ function init() {
   };
 
   let color = [
-    "rgba(248,112,116,0.4)",
-    "rgba(248,235,187,0.5)"
+
+    // red beige
+    //"rgba(248,112,116,0.4)",
+    //"rgba(248,235,187,0.5)"
+
+    "rgba(152,119,182,0.5)",
+    "rgba(228,220,242,0.5)"
+
     //"rgba(200,255,200,0.7)",
     //"rgba(255,200,200,0.9)"
   ];
 
-  for (let iy = 0; iy < 4; iy++) {
-    for (let ix = 0; ix < 4; ix++) {
+  let _p = opt.path;
 
-      let parity = (ix+iy)%2;
+  //let _p = [ [0,0], [0,1], [1,1], [1,0], [2,0], [2,1] ];
+
+  let fudge_parity = ((nx*ny)%2);
+  if ("initial_parity" in opt) {
+    fudge_parity = opt.initial_parity;
+  }
+
+  for (let iy = 0; iy < ny; iy++) {
+    for (let ix = 0; ix < nx; ix++) {
+
+      let parity = (ix+iy +fudge_parity)%2;
 
       let x = sx + ix*x_len;
       let y = sy + iy*y_len;
       let rect = new Two.Rectangle( x,y, x_len, y_len );
-      rect.linewidth = 0;
+      rect.linewidth = 0.5;
       rect.stroke = "rgba(0,0,0,0.4)";
       //rect.fill = "rgba(255, 200, 200, 1)";
       rect.fill = "rgba(255, 200, 200, 1)";
       rect.fill = color[ parity ];
       two.add(rect);
 
-
-
     }
   }
 
+  let path_r = 8.5;
+  let path_color = "rgba(60,60,120,1)";
+
+
+  //path_color = "rgba(180,170,239,1)";
+
+  // this
+  //path_color = "rgba(118,114,223,1)";
+  path_color = "rgba(80,80,140,1)";
+
+  //path_color = "rgba(159,226,235,1)";
+  //path_color = "rgba(107,202,215,1)";
+
+  //path_color = "rgba(166,197,237,1)";
+  //path_color = "rgba(117,162,219,1)";
+
+  let w_p = [];
+  for (let idx = 0; idx < _p.length; idx++) {
+    w_p.push( [ sx + _p[idx][0]*x_len, sy + _p[idx][1]*y_len ] );
+
+    //let path_step = two.makeCircle( w_p[idx][0], w_p[idx][1], path_r) ;
+    //path_step.fill = path_color;
+    //path_step.linewidth = 0;
+    //two.add( path_step );
+
+  }
+
+  let np_center = {};
+  if (("nopath" in opt) &&
+      (opt.nopath)) {
+    np_center = w_p.pop();
+  }
+
+  let tv_path = makeTwoVector(w_p);
+  let t_path = two.makePath(tv_path, true);
+  t_path.fill = "rgba(0,0,0,0)";
+  t_path.linewidth = 10;
+  t_path.stroke = path_color;
+  t_path.cap = 'round';
+  t_path.join = 'round';
+
+  //two.add(t_path);
+
+  let start_circle = two.makeCircle( w_p[0][0], w_p[0][1], path_r );
+  start_circle.fill = path_color;
+  //start_circle.fill = "rgba(120,120,240,1)";
+  //start_circle.fill = "rgba(180,240,180,1)";
+  //start_circle.fill = "rgba(180,240,180,1)";
+  start_circle.linewidth = 0;
+  start_circle.stroke = path_color;
+
+  if (("end_circle" in opt) &&
+      (opt.end_circle)) {
+    let n = w_p.length-1;
+    let end_circle = two.makeCircle( w_p[n][0], w_p[n][1], path_r );
+    //end_circle.fill = "rgba(120,120,240,1)";
+    end_circle.fill = path_color;
+    end_circle.linewidth = 0;
+    end_circle.stroke = path_color;
+  }
+
+  if (("nopath" in opt) &&
+      (opt.nopath)) {
+
+    let alpha = [
+      0.8*Math.sqrt(2)*x_len/8,
+      0.8*Math.sqrt(2)*y_len/8
+    ];
+
+    let co = "rgba(255,81,53,1)";
+    co = "rgba(255,110,88,1)";
+    co = "rgba(241,3,31,1)";
+    co = "rgba(245,52,74,1)";
+    co = "rgba(252,74,3,1)";
+    co = "rgba(252,41,3,1)";
+
+    co  = "rgba(162,47,26,1)";
+    co  = "rgba(255,40,0,1)";
+    co  = "rgba(250,42,0,.9)";
+    co  = "rgba(236,0,51,0.99)";
+    co  = "rgba(190,0,41,1)";
+
+    co = "rgba(252,37,3,0.95)"; //!
+
+    let lw = 8;
+
+    let pnt_cross_a = [
+      [np_center[0] + alpha[0], np_center[1] + alpha[1]],
+      [np_center[0] - alpha[0], np_center[1] - alpha[1]]
+    ];
+    let cross_a = makeTwoVector(pnt_cross_a);
+
+    let ca = two.makePath(cross_a);
+    ca.linewidth = lw;
+    ca.stroke = co;
+    ca.cap = 'round';
+    ca.join = 'round';
+
+    let pnt_cross_b = [
+      [np_center[0] + alpha[0], np_center[1] - alpha[1]],
+      [np_center[0] - alpha[0], np_center[1] + alpha[1]]
+    ];
+    let cross_b = makeTwoVector(pnt_cross_b);
+
+    let cb = two.makePath(cross_b);
+    cb.linewidth = lw;
+    cb.stroke = co;
+    cb.cbp = 'round';
+    cb.join = 'round';
+
+  }
+
+
   two.update();
+}
+
+function init() {
+  let two = g_fig_ctx.two;
+
+  var ele = document.getElementById("fig");
+  two.appendTo(ele);
+
+  //two.update();
+
+  let square_size = 50;
+
+
+  // y+ is downwards
+  //
+  let sx = 50,
+      sy = 50,
+      cur_x = 50,
+      cur_y = 50,
+      dx = 200,
+      dy = 200;
+
+  // path creep fudge
+  let pcf = 0.35;
+  pcf = 0.45;
+
+  //cur_x += dx;
+  cur_x = sx;
+  let opt_2x2_ul = {
+    "sx": cur_x, "sy": cur_y,
+    "nx": 2, "ny": 2,
+    "x_len": square_size, "y_len": square_size,
+    "path": [ [0,1], [1,1], [1,0], [0,0] ]
+  };
+  mk_checkerboard_path(opt_2x2_ul);
+  cur_x += dx - (3*square_size/2);
+
+  let opt_2x2_lr = {
+    "sx": cur_x, "sy": cur_y,
+    "nx": 2, "ny": 2,
+    "x_len": square_size, "y_len": square_size,
+    "path": [ [0,1], [0,0], [1,0], [1,1] ]
+  };
+  mk_checkerboard_path(opt_2x2_lr);
+  cur_x += dx - (3*square_size/2);
+
+  let opt_2x2_np = {
+    "sx": cur_x, "sy": cur_y,
+    "nx": 2, "ny": 2,
+    "nopath": true,
+    "x_len": square_size, "y_len": square_size,
+    "path": [ [0,1], [0,pcf],  [1,0] ]
+    //"path": [ [0,1], [0,0], [1,0], [1,1] ]
+  };
+  mk_checkerboard_path(opt_2x2_np);
+  //cur_x += dx - square_size;
+
+  cur_x = sx;
+  cur_y += dy - (3*square_size/2);
+  let opt_2x3_ur = {
+    "sx": cur_x, "sy": cur_y,
+    "nx": 2, "ny": 3,
+    "initial_parity": 1,
+    "x_len": square_size, "y_len": square_size,
+    "path": [ [0,2], [1,2], [1,1], [0,1], [0,0], [1,0] ]
+  };
+  mk_checkerboard_path(opt_2x3_ur);
+  cur_x += dx - (3*square_size/2);
+
+  let opt_2x3_lr= {
+    "sx": cur_x, "sy": cur_y,
+    "nx": 2, "ny": 3,
+    "initial_parity": 1,
+    "x_len": square_size, "y_len": square_size,
+    "path": [ [0,2], [0,1], [0,0], [1,0], [1,1], [1,2] ]
+  };
+  mk_checkerboard_path(opt_2x3_lr);
+  cur_x += dx - (3*square_size/2);
+
+  let opt_2x3_np = {
+    "sx": cur_x, "sy": cur_y,
+    "nx": 2, "ny": 3,
+    "nopath": true,
+    "initial_parity": 1,
+    "x_len": square_size, "y_len": square_size,
+    "path": [ [0,2], [1,2], [1,1+pcf], [0,0] ]
+    //"path": [ [0,2], [0,1], [0,0], [1,0], [1,1], [1,2] ]
+  };
+  mk_checkerboard_path(opt_2x3_np);
+  cur_x += dx;
+
+
+  cur_x = sx;
+  cur_y += dy - (1*square_size/2);
+  let opt_3x2_ur = {
+    "sx": cur_x, "sy": cur_y,
+    "nx": 3, "ny": 2,
+    "x_len": square_size, "y_len": square_size,
+    "path": [ [0,1], [0,0], [1,0], [1,1], [2,1], [2,0] ]
+  };
+  mk_checkerboard_path(opt_3x2_ur);
+  cur_x += dx - (square_size/2);
+
+  let opt_3x2_ul = {
+    "sx": cur_x, "sy": cur_y,
+    "nx": 3, "ny": 2,
+    "x_len": square_size, "y_len": square_size,
+    "path": [ [0,1], [1,1], [2,1], [2,0], [1,0], [0,0] ]
+  };
+  mk_checkerboard_path(opt_3x2_ul);
+  cur_x += dx - (square_size/2);
+
+  let opt_3x2_np = {
+    "sx": cur_x, "sy": cur_y,
+    "nx": 3, "ny": 2,
+    "nopath": true,
+    "x_len": square_size, "y_len": square_size,
+    "path": [ [0,1], [0,0], [1-pcf,0], [2,1] ]
+    //"path": [ [0,1], [1,1], [2,1], [2,0], [1,0], [0,0] ]
+  };
+  mk_checkerboard_path(opt_3x2_np);
+  cur_x += dx;
+
+
+
+  cur_x = sx;
+  cur_y += dy - (3*square_size/2);
+  let opt_3x3_ur = {
+    "sx": cur_x, "sy": cur_y,
+    "nx": 3, "ny": 3,
+    "x_len": square_size, "y_len": square_size,
+    "path": [
+      [0,2], [0,1], [0,0],
+      [1,0], [1,1], [1,2],
+      [2,2], [2,1], [2,0]
+      ]
+  };
+  mk_checkerboard_path(opt_3x3_ur);
+  cur_x += dx - (square_size/2);
+
+  let opt_3x3_ul = {
+    "sx": cur_x, "sy": cur_y,
+    "nx": 3, "ny": 3,
+    "x_len": square_size, "y_len": square_size,
+    "path": [
+      [0,2], [0,1], [1,1], [1,2],
+      [2,2], [2,1], [2,0], [1,0], [0,0]
+      ]
+  };
+  mk_checkerboard_path(opt_3x3_ul);
+  cur_x += dx - (square_size/2);
+
+  let opt_3x3_lr = {
+    "sx": cur_x, "sy": cur_y,
+    "nx": 3, "ny": 3,
+    "x_len": square_size, "y_len": square_size,
+    "path": [
+      [0,2], [1,2], [1,1], [0,1],
+      [0,0], [1,0], [2,0], [2,1], [2,2]
+      ]
+  };
+  mk_checkerboard_path(opt_3x3_lr);
 
 
 }
