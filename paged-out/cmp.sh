1020debcd60566827f88d11af186e4058229b0b9
zzyzek <zzyzek@thumpernet.com>
2025-12-09 22:15:14 -0500

    libertine font edition

--- paged-out/cmp.sh
+++ paged-out/cmp.sh
@@ -2,6 +2,7 @@
 
 rm -f *.log *.aux *.abs *.log *.out *.xmpdata *.xmpi *.bbl *.blg
 pdflatex PO_BMS.tex
+pdflatex PO_BMS_font_libertine.tex
 #bibtex PO_BMS
 #pdflatex PO_BMS.tex
 #pdflatex PO_BMS.tex
