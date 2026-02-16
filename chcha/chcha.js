f134e03a0cac95ed7852bd93eac610c2c23bd607
zzyzek <zzyzek@thumpernet.com>
2026-02-16 14:36:16 -0500

    wip

--- chcha/chcha.js
+++ chcha/chcha.js
@@ -21,6 +21,10 @@ function print_point(P) {
   }
 }
 
+function chcha_debug_print(ctx) {
+  print_point(ctx.P);
+}
+
 function init_point(N) {
   let P = [];
   for (let i=0; i<N; i++) {
@@ -63,38 +67,246 @@ function _time(p,q,r) {
   return _num / _den;
 }
 
-// Q - even list, sorted by event time, entries of which are indices into point list P
-// Q_l - left event list, sorted by event time
-// Q_r - right even tlist, sorted by event time
-// u_idx - index
-function chcha(ctx) {
-  let Q = [];
+function _turn(p,q,r) {
+  let _den = ((q[0] - p[0])*(r[1] - p[1])) - ((r[0] - p[0])*(q[1] - p[1]));
+  return _den;
+}
 
-  let Q_l = ctx.Q_l;
-  let Q_r = ctx.Q_r;
+function chcha_idx3(ctx, idx3, idx) {
+  if (idx < 0) { idx3[0] = -1; idx3[1] = -1, idx3[2] = -1; return; }
 
-  let u_idx,v_idx;
+  idx3[0] = ctx.H_nei[idx][0];
+  idx3[1] = idx;
+  idx3[2] = ctx.H_nei[idx][1];
 
-  let l_idx = 0,
-      r_idx = 2*n2,
-      q_idx = 0,
-      t_prv = T_0,
-      t_cur = T_0;
-  while (1) {
+  return idx3;
+}
 
-    let t6 = [
-      time3(Q_l, l_idx),
-      time3(Q_r, r_idx),
+function chcha_turn3(ctx, idx3) {
+  if ((idx3[0] < 0) ||
+      (idx3[1] < 0) ||
+      (idx3[2] < 0)) { return 1.0; }
 
-    ];
+  return _turn( ctx.P[idx3[0]], ctx.P[idx3[1]], ctx.P[idx3[2]] );
+}
 
-    t_prv = t_cur;
+function chcha_time3(ctx, idx3) {
+  if ((idx3[0] < 0) ||
+      (idx3[1] < 0) ||
+      (idx3[2] < 0)) { return 1.0; }
+
+  return _time( ctx.P[idx3[0]], ctx.P[idx3[1]], ctx.P[idx3[2]] );
+}
+
+//WIP!!!
+// t_cur, t_nxt need to have correct values
+// last rewind/update of current hull needs to happen
+//
+
+//
+// ctx:
+//
+// P - point list
+// Q - event list, sorted by event time, entries of which are indices into point list P
+//     current event list is q_idx
+// H_nei - point list with index pointers to neighbors for implicit lower hull
+//         0 idx -> -x (left)
+//         1 idx -> +x (right)
+//         -1 indicates no neighbor
+//
+function chcha_recur(ctx, s_idx, e_idx_ni, q_idx) {
+
+  let idx_mid = -1,
+      idx_u = -1,
+      idx_v = -1;
+  let t6 = [0,0,0,0,0,0],
+      t_prv = 0,
+      t_cur = 0;
+  let min_idx = -1,
+      min_val = 0;
+  let _r = -1;
+
+  let idx_u3 = [-1,-1,-1],
+      idx_v3 = [-1,-1,-1],
+      idx_l3 = [-1,-1,-1],
+      idx_r3 = [-1,-1,-1],
+
+      idx_u0u1v1 = [ -1,-1,-1 ],
+      idx_u1u2v1 = [ -1,-1,-1 ],
+
+      idx_u1v0v1 = [ -1,-1,-1 ],
+      idx_u1v1v2 = [ -1,-1,-1 ];
+
+  let n = (e_idx_ni - s_idx);
+  let n2 = Math.floor(n/2);
+  let idx_mid = s_idx + n2;
+
+  let iq_cur = q_idx;
+  let iq_nxt = 1-q_idx;
+
+  if (n==1) {
+    let idx = ctx.Q[iq_cur][s_idx];
+    ctx.H_nei[idx][0] = -1;
+    ctx.H_nei[idx][1] = -1;
+    return 0;
   }
 
+  _r = chcha_recur(ctx, s_idx, idx_mid, iq_nxt);
+  if (_r < 0) { return _r; }
+
+  _r = chcha_recur(ctx, idx_mid, e_idx_ni, iq_nxt);
+  if (_r < 0) { return _r; }
+
+  idx_u = idx_mid;
+  idx_v = idx_mid+1;
+
+  do {
+
+    chcha_idx3(ctx, idx_u3, idx_u);
+    chcha_idx3(ctx, idx_v3, idx_v);
+
+    // move v right
+    //
+    if (chcha_turn3(ctx, [ idx_u3[1], idx_v3[1], idx_v3[2] ] ) < 0) {
+      idx_v = idx_v3[2];
+    }
+
+    // move u left
+    //
+    else if (chcha_turn3(ctx, [ idx_u3[0], idx_u3[1], idx_v3[1] ] ) < 0) {
+      idx_u = idx_u3[0];
+    }
+
+    else { break; }
+
+  } while (1);
+
+  let t_cur = -1;
+  let t_nxt = 1;
+
+  let q_n_idx = 0;
+  let q_l_idx = s_idx;
+  let q_r_idx = idx_mid;
+  while (1) {
+
+    chcha_idx3(ctx, idx_u3, idx_u);
+    chcha_idx3(ctx, idx_v3, idx_v);
+    chcha_idx3(ctx, idx_l3, ctx.Q[iq_cur][q_l_idx]);
+    chcha_idx3(ctx, idx_r3, ctx.Q[iq_cur][q_r_idx]);
+
+    t[0] = chcha_time3(ctx, idx_l3);
+    t[1] = chcha_time3(ctx, idx_r3);
+
+    t[2] = chcha_time3(ctx, [idx_u3[1], idx_u3[2], idx_v3[1]]);
+    t[3] = chcha_time3(ctx, [idx_u3[0], idx_u3[1], idx_v3[1]]);
+
+    t[4] = chcha_time3(ctx, [idx_u3[1], idx_v3[0], idx_v3[1]]);
+    t[5] = chcha_time3(ctx, [idx_u3[1], idx_v3[1], idx_v3[2]]);
+
+    let tm_idx = -1;
+    for (let _i=0; _i<6; _i++) {
+      if ((t[_i] > t_cur) &&
+          (t[_i] < t_nxt)) {
+        t_nxt = t[_i];
+        tm_idx = _i;
+      }
+    }
+    if (tm_idx == -1) { break; }
+
+    // event is left of bridge
+    //
+    else if (tm_idx == 0) {
+      if (ctx.P[idx_l3[1]][0] < ctx.P[idx_u3[1]][0]) {
+        ctx.Q[iq_nxt][q_n_idx] = ctx.Q[iq_cur][q_l_idx];
+        chcha_H_nei_update(ctx, ctx.Q[iq_nxt][q_n_idx]);
+        q_l_idx++;
+      }
+    }
+
+    // event is right of bridge
+    //
+    else if (tm_idx == 1) {
+      if (ctx.P[idx_r3[1]][0] > ctx.P[idx_v3[1]][0]) {
+        ctx.Q[iq_nxt][q_n_idx] = ctx.Q[iq_cur][q_r_idx];
+        chcha_H_nei_update(ctx, ctx.Q[iq_nxt][q_n_idx]);
+        q_r_idx++;
+      }
+    }
+
+    // (u,u^+,v)
+    // u <- u^+
+    //
+    else if (tm_idx == 2) {
+      ctx.Q[iq_nxt][q_n_idx] = idx_u3[2];
+      idx_u = idx_u3[2];
+    }
+
+    // (u^-,u,v)
+    // u <- u^-
+    //
+    else if (tm_idx == 3) {
+      ctx.Q[iq_nxt][q_n_idx] = idx_u3[1];
+      idx_u = idx_u3[0];
+    }
+
+    // (u,v^-,v)
+    // v <- v^-
+    //
+    else if (tm_idx == 4) {
+      ctx.Q[iq_nxt][q_n_idx] = idx_v3[0];
+      idx_v = idx_v3[0];
+    }
+
+    // (u,v,v^+)
+    // v <- v^+
+    //
+    else if (tm_idx == 5) {
+      ctx.Q[iq_nxt][q_n_idx] = idx_v3[1];
+      idx_v = idx_v3[2];
+    }
+
+    q_n_idx++;
+
+  }
+  ctx.Q[iq_nxt][q_n_idx] = -1;
+
+
+  ctx.H_nei[ u_idx ][1] = v_idx;
+  ctx.H_nei[ v_idx ][0] = u_idx;
+
+  chcha_idx3( ctx, idx_u3, idx_u );
+  chcha_idx3( ctx, idx_v3, idx_v );
+
+  for (let k = (q_n_idx-1); k >= 0; k--) {
+  }
 
 }
 
+
 let P = init_point(20);
 P.sort(pnt_cmp);
+
+function chcha_init(P) {
+  let n = P.length;
+
+  P.sort(pnt_cmp);
+
+  let ctx = {
+    "p" : P,
+    "q_idx": 0,
+    "Q": [ [], [] ],
+    "H_nei" : []
+  };
+
+
+  for (let i=0; i<n; i++) {
+    ctx.Q[0].push(-1);
+    ctx.Q[1].push(-1);
+    ctx.H_nei.push( [-1,-1] );
+  }
+
+  return ctx;
+}
+
 print_point(P);
 
